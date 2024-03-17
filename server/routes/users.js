6d0773ce6c499812f9e608d220ca2704d65d3f92
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-03-17 15:48:01 +0100

    Add Update/Get/Delete functions

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { getUser, update, deleteUser } from "../controllers/user.js";
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
 
+// Update User
+router.put("/:id", verifyToken, update);
 
-router.get("/find/id:", (req, res) => {
-   
-});
+
+// Get User
+router.get("/find/:id", getUser);
+
+// Delete User
+router.delete("/:id", verifyToken, deleteUser);
 
 export default router;
